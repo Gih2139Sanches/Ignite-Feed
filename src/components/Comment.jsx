955795b24cbf9cc877f1c109e290e5f243e72ae4
Gih2139Sanches <giovanaclemente2139@gmail.com>
2023-12-04 18:25:40 -0300

    Commit base do projeto

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { Trash } from '@phosphor-icons/react/dist/ssr';
+import styles from './Comment.module.css'
+import { ThumbsUp } from '@phosphor-icons/react';
+
+export function Comment(){
+    return (
+        <div className={styles.comment}>
+            <img src="https://github.com/gih2139sanches.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Giovana Sanches</strong>
+                            <time title='11 de maio ás 08:13h' dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
+                        </div>
+                        <button title='deletar comentário'>
+                            <Trash size={20}/>
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp size={20} />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    );
+}
