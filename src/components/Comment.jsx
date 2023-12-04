63c5a63a907d7316805f9ffb38ac1ee45fce5be3
Gih2139Sanches <giovanaclemente2139@gmail.com>
2023-12-04 18:46:06 -0300

    Estilização dos comentários

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -15,7 +15,7 @@ export function Comment(){
                             <time title='11 de maio ás 08:13h' dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
                         </div>
                         <button title='deletar comentário'>
-                            <Trash size={20}/>
+                            <Trash size={24}/>
                         </button>
                     </header>
 
