96a43ff458b574735408b8e091f5a884ebd851d0
Gih2139Sanches <giovanaclemente2139@gmail.com>
2023-12-06 19:15:43 -0300

    Validações do publicar e realizando likes nos comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -29,7 +29,12 @@ export function Post({author, publishedAt, content}){
     }
 
     function handleNewCommentChange(){
+        event.target.setCustomValidity('');
         setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInValid(){
+        event.target.setCustomValidity('Esse campo é obrigatório!');
     }
 
     function deleteComment(commentToDelete){
@@ -39,6 +44,8 @@ export function Post({author, publishedAt, content}){
 
         setComments(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length == 0
 
     return(
         <article className={styles.post}>
@@ -69,9 +76,13 @@ export function Post({author, publishedAt, content}){
                     placeholder='Escreva um comentário...'
                     value={newCommentText}
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInValid}
+                    required
                 />
                 <footer>
-                    <button type='submit'>Publicar</button>
+                    <button type='submit' disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>
             </form>
             <div className={styles.commentList}>
